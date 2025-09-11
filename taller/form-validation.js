b4cdd08c644d9d9bbe325937e67cd2eb223f76fa
LLuisGL <LuisGoncalvesLopez@gmail.com>
2025-09-11 18:43:44 -0500

    uploading files

--- taller/form-validation.js
+++ taller/form-validation.js
@@ -1,20 +1,49 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
+const forms = document.querySelectorAll('.needs-validation')
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+let id = 0;
 
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
+const loginForm = document.getElementById('submit')
+const tableBody = document.getElementById('tbody')
+const counter = document.getElementById('counter')
 
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
+loginForm.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const firstName = document.getElementById('firstName');
+  const lastName = document.getElementById('lastName');
+  const email = document.getElementById('email');
+  const country = document.getElementById('country');
+  const check = document.getElementById('same-address');
+
+  
+  if (
+    firstName.value === '' || 
+    lastName.value === '' || 
+    email.value === '' || 
+    country.value === 'Choose...'
+  ) {
+    alert("Rellene todos los campos");
+    return;
+  }
+
+  id++;
+
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${id}</td>
+    <td>${firstName.value}</td>
+    <td>${lastName.value}</td>
+  `;
+
+  tableBody.appendChild(row);
+
+  counter.innerHTML = id;
+  firstName.value = '';
+  lastName.value = '';
+  email.value = '';
+  country.value = 'Choose...';
+  check.checked = false;
+});
+
+
+
